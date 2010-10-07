b3f071c2b80feaee56ca99d08cf130c042e8152f
Toshiaki Maki <makingx@gmail.com>
2010-10-08 01:50:45 +0900

    support theme

--- src/main/clojure/am/ik/categolj/core.clj
+++ src/main/clojure/am/ik/categolj/core.clj
@@ -1,25 +1,40 @@
 (ns am.ik.categolj.core
   (:use [ring.adapter.jetty])
-  (:use [ring.middleware.reload])
-  (:require [net.cgrand.enlive-html :as html]))
+  (:use [ring.middleware reload stacktrace static])
+  (:require [net.cgrand.enlive-html :as html])
+  (:require [clojure.java.io :as io]))
 
-          
+(defn ^java.net.URL get-resource [f]
+  (.getResource (.getContextClassLoader (Thread/currentThread)) f))
+
+(def *config* (read-string (slurp (get-resource "config.clj"))))
+
+(def *theme-dir* (str "theme/" (:theme *config*)))
+
 (html/deftemplate categolj-layout
-  "pages/index.html"
+  (str *theme-dir* "/pages/index.html")
   [title]
   [:title] (html/content title)
   [:h1/a] (html/do-> (html/content title) (html/set-attr :href "http://github.com/making/categolj"))
   [:p#feature] (html/content "CategoLJ is a Simple Blog System written by Clojure featuring categories of the articles."))
 
-(defn hello [req] 
+(defn hello [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (categolj-layout "CategoLJ")})
+   :body (categolj-layout (:title *config*))})
+
+(defn charset-filter [app]
+  (fn [req]
+    (let [orig (app req)]
+      (assoc orig :charset (:charset *config*)))))
 
 (def app
-     (wrap-reload #'hello '(am.ik.categolj.core)))
+     (-> #'hello
+         (charset-filter)
+         (wrap-stacktrace)
+         (wrap-static (.getPath (get-resource (str *theme-dir* "/public/"))) (:static-dir *config*))
+         (wrap-reload '(am.ik.categolj.core))
+         ))
 
 (defn boot [& [port]]
-  (run-jetty app {:port (if port (Integer/parseInt port) 8080)}))
-
-
+  (run-jetty app {:port (:port *config*)}))

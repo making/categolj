7d080ea7a14365917d5c4f20c85c763aa7dc521c
making <makingx@gmail.com>
2010-12-12 21:42:00 +0530

    escape nullpo

--- src/main/clojure/am/ik/categolj/utils/date_utils.clj
+++ src/main/clojure/am/ik/categolj/utils/date_utils.clj
@@ -1,8 +1,10 @@
 (ns am.ik.categolj.utils.date-utils)
 
 (defn format-date [^java.util.Date date]
-  (.format (java.text.SimpleDateFormat. "yyyy/MM/dd HH:mm:ss") date))
+  (if date
+    (.format (java.text.SimpleDateFormat. "yyyy/MM/dd HH:mm:ss") date) ""))  
 
 (defn parse-date [str]
-  (let [sdf (java.text.SimpleDateFormat. "yyyy/MM/dd HH:mm:ss")]
-    (.parse sdf str)))
+  (if str
+    (let [sdf (java.text.SimpleDateFormat. "yyyy/MM/dd HH:mm:ss")]
+      (.parse sdf str))))
